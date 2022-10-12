5e92db3bef66733d4512b0708d4f5220c12f0253
Dmitriy Panfilyonok <dmitriy.panfilyonok@gmail.com>
2022-10-12 22:41:59 +0300

    Fix npm build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     root: "src/Client.React",
     build: {
-        outDir: "../public",
+        outDir: "./public",
         emptyOutDir: true,
         sourcemap: true
     }
